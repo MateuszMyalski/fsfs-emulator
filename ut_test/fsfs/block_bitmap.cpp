389d6926cb3cac8ba1c32b79e60db4f51b73dd68
Mateusz Myalski <mateusz.myalski+github@gmail.com>
2021-08-22 21:47:24 +0200

    UT refactor

--- ut_test/fsfs/block_bitmap.cpp
+++ ut_test/fsfs/block_bitmap.cpp
@@ -15,68 +15,88 @@ class BlockBitmapTest : public ::testing::TestWithParam<fsize>, public TestBaseB
     void TearDown() override { delete bitmap; }
 };
 
-TEST_P(BlockBitmapTest, get_data_block_throw) {
+TEST_P(BlockBitmapTest, get_block_status_throw_bitmap_not_initialized) {
     EXPECT_THROW(bitmap->get_block_status(n_blocks), std::runtime_error);
-
-    bitmap->init(n_blocks);
-
-    EXPECT_THROW(bitmap->get_block_status(n_blocks), std::invalid_argument);
-    EXPECT_THROW(bitmap->get_block_status(-1), std::invalid_argument);
-
-    bitmap->get_block_status(n_blocks - 1);
 }
 
-TEST_P(BlockBitmapTest, set_and_get_map_line_border) {
+TEST_P(BlockBitmapTest, get_block_status_throw_overflow_bitmap_size) {
+    bitmap->init(n_blocks);
+    EXPECT_THROW(bitmap->get_block_status(n_blocks), std::invalid_argument);
+}
+
+TEST_P(BlockBitmapTest, get_block_status_throw_negative_value) {
+    bitmap->init(n_blocks);
+    EXPECT_THROW(bitmap->get_block_status(-1), std::invalid_argument);
+}
+
+TEST_P(BlockBitmapTest, get_block_status_valid_value) {
+    bitmap->init(n_blocks);
+    EXPECT_THROW(bitmap->get_block_status(-1), std::invalid_argument);
+}
+
+TEST_P(BlockBitmapTest, get_data_block_throw) {
+    bitmap->init(n_blocks);
+    EXPECT_NO_THROW(bitmap->get_block_status(n_blocks - 1));
+}
+
+TEST_P(BlockBitmapTest, set_and_get_multiple_set) {
     bitmap->init(n_blocks);
 
-    EXPECT_EQ(bitmap->get_block_status(0), 0);
-
-    EXPECT_EQ(bitmap->get_block_status(bitmap_row_length - 1), 0);
-
-    bitmap->set_block(bitmap_row_length, 1);
-
-    EXPECT_EQ(bitmap->get_block_status(bitmap_row_length), 1);
-
-    EXPECT_EQ(bitmap->get_block_status(bitmap_row_length + 1), 0);
-
-    EXPECT_EQ(bitmap->get_block_status(0), 0);
-
-    EXPECT_EQ(bitmap->get_block_status(bitmap_row_length - 1), 0);
+    ASSERT_FALSE(bitmap->get_block_status(0));
+    ASSERT_FALSE(bitmap->get_block_status(bitmap_row_length));
+    EXPECT_FALSE(bitmap->get_block_status(bitmap_row_length + 1));
+    ASSERT_FALSE(bitmap->get_block_status(bitmap_row_length - 1));
 
     bitmap->set_block(bitmap_row_length, 1);
 
-    EXPECT_EQ(bitmap->get_block_status(bitmap_row_length), 1);
+    EXPECT_FALSE(bitmap->get_block_status(0));
+    EXPECT_TRUE(bitmap->get_block_status(bitmap_row_length));
+    EXPECT_FALSE(bitmap->get_block_status(bitmap_row_length + 1));
+    EXPECT_FALSE(bitmap->get_block_status(bitmap_row_length - 1));
 
-    EXPECT_EQ(bitmap->get_block_status(bitmap_row_length + 1), 0);
+    bitmap->set_block(bitmap_row_length, 1);
+
+    EXPECT_FALSE(bitmap->get_block_status(0));
+    EXPECT_TRUE(bitmap->get_block_status(bitmap_row_length));
+    EXPECT_FALSE(bitmap->get_block_status(bitmap_row_length + 1));
+    EXPECT_FALSE(bitmap->get_block_status(bitmap_row_length - 1));
 }
 
 TEST_P(BlockBitmapTest, set_and_get_block) {
     bitmap->init(n_blocks);
 
     bitmap->set_block(block_size - 1, 1);
-    EXPECT_EQ(bitmap->get_block_status(block_size - 1), 1);
+    EXPECT_TRUE(bitmap->get_block_status(block_size - 1));
 
     bitmap->set_block(block_size + 1, 1);
-    EXPECT_EQ(bitmap->get_block_status(block_size + 1), 1);
-
-    EXPECT_EQ(bitmap->get_block_status(block_size + 2), 0);
+    EXPECT_TRUE(bitmap->get_block_status(block_size - 1));
+    EXPECT_TRUE(bitmap->get_block_status(block_size + 1));
+    EXPECT_FALSE(bitmap->get_block_status(block_size + 2));
 
     bitmap->set_block(block_size + 1, 0);
-    EXPECT_EQ(bitmap->get_block_status(block_size + 1), 0);
+    EXPECT_TRUE(bitmap->get_block_status(block_size - 1));
+    EXPECT_FALSE(bitmap->get_block_status(block_size + 1));
+    EXPECT_FALSE(bitmap->get_block_status(block_size + 2));
 }
 
-TEST_P(BlockBitmapTest, next_free_throw) {
+TEST_P(BlockBitmapTest, next_free_throw_uninitialized) {
     EXPECT_THROW(bitmap->next_free(-1), std::invalid_argument);
     EXPECT_THROW(bitmap->next_free(0), std::runtime_error);
+}
 
+TEST_P(BlockBitmapTest, next_free_throw_invalid_argument) {
     bitmap->init(n_blocks);
 
     EXPECT_THROW(bitmap->next_free(-1), std::invalid_argument);
+}
+
+TEST_P(BlockBitmapTest, next_free_throw_overflow_bitmap_size) {
+    bitmap->init(n_blocks);
     EXPECT_THROW(bitmap->next_free(n_blocks), std::runtime_error);
     EXPECT_THROW(bitmap->next_free(n_blocks + 1), std::runtime_error);
 }
 
-TEST_P(BlockBitmapTest, next_free) {
+TEST_P(BlockBitmapTest, next_free_basic_set) {
     bitmap->init(n_blocks);
 
     EXPECT_EQ(bitmap->next_free(0), 0);
@@ -87,6 +107,10 @@ TEST_P(BlockBitmapTest, next_free) {
     bitmap->set_block(2, 1);
 
     EXPECT_EQ(bitmap->next_free(0), 3);
+}
+
+TEST_P(BlockBitmapTest, next_free_in_next_row) {
+    bitmap->init(n_blocks);
 
     for (auto i = 0; i < bitmap_row_length; i++) {
         bitmap->set_block(i, 1);
@@ -94,6 +118,10 @@ TEST_P(BlockBitmapTest, next_free) {
 
     bitmap->set_block(bitmap_row_length - 5, 0);
     EXPECT_EQ(bitmap->next_free(0), bitmap_row_length - 5);
+}
+
+TEST_P(BlockBitmapTest, next_free_no_free) {
+    bitmap->init(n_blocks);
 
     for (auto i = 0; i < n_blocks; i++) {
         bitmap->set_block(i, 1);
