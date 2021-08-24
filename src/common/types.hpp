408d3bcd6087d0586d59abfede510a44d53e9b2c
Mateusz Myalski <mateusz.myalski+github@gmail.com>
2021-08-24 19:59:02 +0200

    Added reinterpret cast to data

--- src/common/types.hpp
+++ src/common/types.hpp
@@ -5,6 +5,12 @@ namespace FSFS {
 typedef int32_t address;
 typedef int32_t fsize;
 typedef uint8_t data;
+
+template <typename T>
+data* cast_to_data(T container) {
+    return reinterpret_cast<data*>(container);
+}
+
 }
 
 #endif
